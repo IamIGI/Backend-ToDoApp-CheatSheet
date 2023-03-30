093591335bff958edae16ed344e28bf49bedacdd
i.kłusek <J6ZkqwVUn>
2023-03-30 19:50:17 +0200

    added isDone property AngularToDoApp

--- src/model/ToDoList.ts
+++ src/model/ToDoList.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 export interface ToDoObject {
-    userName: string;
-    title: string;
-    date?: string;
+  userName: string;
+  title: string;
+  isDone?: boolean;
+  date?: string;
 }
 
 export interface ToDoDocument extends ToDoObject, mongoose.Document {}
@@ -10,9 +11,10 @@ export interface ToDoDocument extends ToDoObject, mongoose.Document {}
 const Schema = mongoose.Schema;
 
 const toDoSchema = new Schema({
-    userName: String,
-    title: String,
-    date: String,
+  userName: String,
+  title: String,
+  isDone: Boolean,
+  date: String,
 });
 
 const ToDoModel = mongoose.model<ToDoDocument>('ToDoLists', toDoSchema);
--- src/services/toDoList.services.ts
+++ src/services/toDoList.services.ts
@@ -4,68 +4,90 @@ import currentDate from '../utils/currentDate';
 
 /** return  all items from to do list */
 async function getToDoList(): Promise<
-    LeanDocument<
-        ToDoDocument & {
-            _id: Types.ObjectId;
-        }
-    >[]
+  LeanDocument<
+    ToDoDocument & {
+      _id: Types.ObjectId;
+    }
+  >[]
 > {
-    const result = await ToDoModel.find({}).sort({ $natural: -1 }).lean();
-    return result;
+  const result = await ToDoModel.find({}).sort({ $natural: -1 }).lean();
+  return result;
 }
 /**Save item to list */
 async function saveToDoItem(
-    object: ToDoObject
+  object: ToDoObject
 ): Promise<{ status: number; message: string; ToDoId?: string; reason?: unknown }> {
-    const date = currentDate.getDate();
-    object.date = date;
-    try {
-        const newToDoItem = new ToDoModel(object);
-        const result = await newToDoItem.save();
+  const date = currentDate.getDate();
+  object.date = date;
+  object.isDone = false;
+  try {
+    const newToDoItem = new ToDoModel(object);
+    const result = await newToDoItem.save();
 
-        return { status: 201, message: 'Item added to ToDo list', ToDoId: result._id };
-    } catch (err) {
-        return { status: 500, message: 'Could not save Item', reason: err };
-    }
+    return { status: 201, message: 'Item added to ToDo list', ToDoId: result._id };
+  } catch (err) {
+    return { status: 500, message: 'Could not save Item', reason: err };
+  }
 }
 
 /** delete item from list */
 async function deleteOne(id: string): Promise<{ status: number; message: string; reason?: unknown }> {
-    try {
-        const isExists = await ToDoModel.findOne({ _id: id }).exec();
-        if (!isExists) return { status: 404, message: 'Could not find given ToDo item' };
+  try {
+    const isExists = await ToDoModel.findOne({ _id: id }).exec();
+    if (!isExists) return { status: 404, message: 'Could not find given ToDo item' };
 
-        await ToDoModel.deleteOne({ _id: id });
-        return { status: 201, message: 'Item deleted' };
-    } catch (err) {
-        return { status: 500, message: 'Could not save Item', reason: err };
-    }
+    await ToDoModel.deleteOne({ _id: id });
+    return { status: 201, message: 'Item deleted' };
+  } catch (err) {
+    return { status: 500, message: 'Could not save Item', reason: err };
+  }
 }
 
 /** edit item from list */
 async function edit(
-    id: string,
-    title: string
+  id: string,
+  title: string
 ): Promise<{ status: number; message: string; id?: string; reason?: unknown }> {
-    const date = currentDate.getDate();
+  const date = currentDate.getDate();
 
-    try {
-        const isExists = await ToDoModel.findOne({ _id: id }).exec();
-        if (!isExists) return { status: 404, message: 'Could not find given ToDo item' };
+  try {
+    const isExists = await ToDoModel.findOne({ _id: id }).exec();
+    if (!isExists) return { status: 404, message: 'Could not find given ToDo item' };
 
-        await ToDoModel.updateOne(
-            { _id: id },
-            {
-                $set: {
-                    title,
-                    date,
-                },
-            }
-        );
+    await ToDoModel.updateOne(
+      { _id: id },
+      {
+        $set: {
+          title,
+          date,
+        },
+      }
+    );
 
-        return { status: 201, message: 'Item successfully edited' };
-    } catch (err) {
-        return { status: 500, message: 'Other Error', reason: err };
-    }
+    return { status: 201, message: 'Item successfully edited' };
+  } catch (err) {
+    return { status: 500, message: 'Other Error', reason: err };
+  }
 }
-export default { getToDoList, saveToDoItem, deleteOne, edit };
+
+async function editIsDone(id: string): Promise<{ status: number; message: string; id?: string; reason?: unknown }> {
+  try {
+    const isExists = await ToDoModel.findOne({ _id: id }).exec();
+    if (!isExists) return { status: 404, message: 'Could not find given ToDo item' };
+
+    await ToDoModel.updateOne(
+      { _id: id },
+      {
+        $set: {
+          isDone: !isExists.isDone,
+        },
+      }
+    );
+
+    return { status: 201, message: 'Item successfully edited' };
+  } catch (err) {
+    return { status: 500, message: 'Other Error', reason: err };
+  }
+}
+
+export default { getToDoList, saveToDoItem, deleteOne, edit, editIsDone };
